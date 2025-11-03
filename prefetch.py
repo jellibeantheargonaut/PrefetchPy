4ea05481480d606817415918df0dba628893a51e
Jellibean <jellibean@Argo.local>
2025-11-03 14:33:52 -0500

    Banner and Library check

---
+++ prefetch.py
@@ -0,0 +1,31 @@
+## A python tool to parse windows prefetch files
+## uses libscca-python library
+
+import os
+import sys
+import argparse
+
+import pyscca
+
+
+BANNER = r"""
+▗▄▄▖ ▗▄▄▖ ▗▄▄▄▖▗▄▄▄▖▗▄▄▄▖▗▄▄▄▖▗▄▄▖▗▖ ▗▖
+▐▌ ▐▌▐▌ ▐▌▐▌   ▐▌   ▐▌     █ ▐▌   ▐▌ ▐▌
+▐▛▀▘ ▐▛▀▚▖▐▛▀▀▘▐▛▀▀▘▐▛▀▀▘  █ ▐▌   ▐▛▀▜▌
+▐▌   ▐▌ ▐▌▐▙▄▄▖▐▌   ▐▙▄▄▖  █ ▝▚▄▄▖▐▌ ▐▌
+                                                                               
+=========================================================
+
+Prefetch:   A python tool to parse windows prefetch files
+Author:     @jellibeantheargonaut
+
+=========================================================
+"""
+
+def main():
+    print(BANNER)
+
+
+
+if __name__ == "__main__":
+    main()
